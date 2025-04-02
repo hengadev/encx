7d454ae2d4ed725b5846538f6c4f3b0aa0a01294
GaryHY <henry.gary@hotmail.com>
2025-04-02 11:11:40 +0200

    feat: simplified encryptor constructor to use DefaultArgon2Params

--- encryptor.go
+++ encryptor.go
@@ -7,32 +7,25 @@ import (
 )
 
 type Encryptor struct {
-	KeyEncryptionKey []byte // 32 bytes for AES-256
-	Pepper           []byte // Additional security for password hashing
-	Argon2Params     *Argon2Params
+	KeyEncryptionKey  []byte // 32 bytes for AES-256
+	Pepper            []byte // Additional security for password hashing
+	Argon2Params      *Argon2Params
 }
 
-func New(encryptionKey string, argon2params *Argon2Params) (*Encryptor, error) {
-	var encryptor Encryptor
+func New(encryptionKey string) (*Encryptor, error) {
 	// Convert hex-encoded key to bytes
 	key, err := hex.DecodeString(encryptionKey)
 	if err != nil {
 		return nil, err
 	}
-	encryptor.KeyEncryptionKey = key
-
 	// Generate random pepper
 	pepper := make([]byte, 32)
 	if _, err := io.ReadFull(rand.Reader, pepper); err != nil {
 		return nil, err
 	}
-	encryptor.Pepper = pepper
-
-	if argon2params != nil {
-		encryptor.Argon2Params = argon2params
-	} else {
-		encryptor.Argon2Params = DefaultArgon2Params()
-	}
-
-	return &encryptor, nil
+	return &Encryptor{
+		KeyEncryptionKey: key,
+		Pepper:           pepper,
+		Argon2Params:     DefaultArgon2Params(),
+	}, nil
 }
