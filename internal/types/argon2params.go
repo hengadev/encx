d542c7e16debae8932f17a6ef4af1528dd0f2051
GaryHY <henry.gary@hotmail.com>
2025-04-19 23:38:20 +0200

    feat: add Argon2Params in types package with Validate function with test
    The Validate function allows to verify that a specific configuration
    respect security requirements

---
+++ internal/types/argon2params.go
@@ -0,0 +1,72 @@
+package types
+
+import (
+	"fmt"
+
+	"github.com/hengadev/errsx"
+)
+
+// TODO: document recommended parameter values in the struct definition
+
+// Argon2Params defines the parameters for Argon2id
+type Argon2Params struct {
+	Memory      uint32
+	Iterations  uint32
+	Parallelism uint8
+	SaltLength  uint32
+	KeyLength   uint32
+}
+
+func NewArgon2Params(
+	memory uint32,
+	iterations uint32,
+	parallelism uint8,
+	saltLength uint32,
+	keyLength uint32,
+) (*Argon2Params, error) {
+	// check for minimum security requirements
+	params := &Argon2Params{
+		Memory:      memory,
+		Iterations:  iterations,
+		Parallelism: parallelism,
+		SaltLength:  saltLength,
+		KeyLength:   keyLength,
+	}
+	if err := params.Validate(); err != nil {
+		return nil, fmt.Errorf("validate Argon2 parameters: %w", err)
+	}
+	return params, nil
+}
+
+// use a map for the errors here
+func (a *Argon2Params) Validate() error {
+	var errs errsx.Map
+	// OWASP recommended minimums as of 2023
+	if a.Memory < 19456 { // 19 MiB minimum
+		errs.Set("memory", "parameter too low, minimum recommended is 19456 KiB")
+	}
+	if a.Iterations < 2 {
+		errs.Set("iterations", "parameter too low, minimum recommended is 2")
+	}
+	if a.Parallelism < 1 {
+		errs.Set("parallelism", "parameter must be at least 1")
+	}
+	if a.SaltLength < 16 {
+		errs.Set("saltLength", "parameter too short, minimum recommended is 16 bytes")
+	}
+	if a.KeyLength < 32 {
+		errs.Set("keyLength", "parameter too short, minimum recommended is 32 bytes")
+	}
+	return errs.AsError()
+}
+
+// DefaultArgon2Params returns recommended parameters for Argon2id
+func DefaultArgon2Params() *Argon2Params {
+	return &Argon2Params{
+		Memory:      64 * 1024, // 64MB
+		Iterations:  3,
+		Parallelism: 2,
+		SaltLength:  16,
+		KeyLength:   32,
+	}
+}
