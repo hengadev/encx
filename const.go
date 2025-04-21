2cf52d748c7e03d7cbd6d33bb3273eb033bdfcf3
GaryHY <henry.gary@hotmail.com>
2025-04-21 12:30:21 +0200

    feat: add const for KEK version tracking and DEKEncrypted fields

--- const.go
+++ const.go
@@ -1,8 +1,10 @@
 package encx
 
 const (
+	VERSION_FIELD          = "KeyVersion"
 	DEK_FIELD              = "DEK"
 	ENCRYPTED_FIELD_SUFFIX = "Encrypted"
+	DEK_ENCRYPTED_FIELD    = DEK_FIELD + ENCRYPTED_FIELD_SUFFIX
 	HASHED_FIELD_SUFFIX    = "Hash"
 	STRUCT_TAG             = "encx"
 )
